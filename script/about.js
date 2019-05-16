485ba92552e06a6c16d761225703305cad78d207
Pawel Jakubiec <pawel143700@gmail.com>
2019-05-16 19:51:51 +0200

    Update 1.3.4 Poprawka tekstu o mnie i animacje dla 900px

--- script/about.js
+++ script/about.js
@@ -1,8 +1,9 @@
 $(function(){
     $(window).on('scroll', function(){
                                             //Responsive
+        var $width = parseInt($(window).width());
 
-        if (parseInt($(window).width()) < 600) {
+        if ($width < 600) {
             //jeden
             if(this.scrollY >150){
                 $('.about_one').addClass('u_slide');
@@ -40,7 +41,44 @@ $(function(){
             }
 
 
+            }                               // Dla max 900
+        else if ($width > 600 && $width < 900){
+            
+            if(this.scrollY >280){
+                $('.about_one').addClass('u_slide');
             }
+            else{
+                $('.about_one').removeClass('u_slide');
+            }
+    //dwa
+            if(this.scrollY > 320){
+                $('.about_two').addClass('u_slide');
+            }
+            else{
+                $('.about_two').removeClass('u_slide');
+            }
+    //trzy
+            if(this.scrollY > 450){
+                $('.about_three').addClass('u_slide');
+            }
+            else{
+                $('.about_three').removeClass('u_slide');
+            }
+    //cztery
+            if(this.scrollY > 550){
+                $('.about_four').addClass('u_slide');
+            }
+            else{
+                $('.about_four').removeClass('u_slide');
+            }
+    //piec
+            if(this.scrollY > 640){
+                $('.about_five').addClass('u_slide');
+            }
+            else{
+                $('.about_five').removeClass('u_slide');
+            }
+        }
                             //Dla duzych
         else{
 //jeden
